b778bb4b6c28d42175a9d7361c4eaba770e4df6c
alex <alamos8@gmail.com>
2012-11-07 15:27:33 +0100

    Funciones fact() y fib()

--- lib/math_games.rb
+++ lib/math_games.rb
@@ -1,3 +1,21 @@
 class MathGames
+
+	#===========================
+	# Factorial
+	#===========================
+	def self.fact(n)
+		raise RuntimeError, 'Argumento debe ser >= 0' unless n >= 0
+		n == 0 ? 1 : n * fact(n-1)
+	end
+
+	#===========================
+	# Fibonacci
+	#===========================
+	def self.fib(n)
+		raise RuntimeError, 'Argumento debe ser >= 0' unless n >= 0
+		n < 2 ? n : fib(n-1) + fib(n-2) 
+	end
+
 end
+
 
