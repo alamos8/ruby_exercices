b86830eefaeae20cc9fffc4666dfa920cfb7c3ca
alex <alamos8@gmail.com>
2012-11-08 16:32:15 +0100

    cion

--- lib/carta.rb
+++ lib/carta.rb
@@ -1,2 +1,25 @@
 class Carta
+
+	@@palos=[:oros, :bastos, :copas, :espadas]
+
+	attr :palo
+	attr :valor
+
+	def initialize(palo_carta, valor_carta)
+
+		raise RuntimeException, "Valor incorrecto" unless valor_carta > 0 && valor_carta < 11
+		raise RuntimeException, 'Palo incorrecto'  unless @@palos.include? palo_carta
+
+    	@palo = palo_carta
+    	@valor = valor_carta
+  	end
+
+  	def mayor_que?(carta)   
+  		if (carta.palo == @palo) then @valor > carta.valor
+  		elsif 
+
+  		end
+  			
+  	end
+
 end
